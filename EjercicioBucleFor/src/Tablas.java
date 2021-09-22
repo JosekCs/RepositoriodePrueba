bc27802a34c08e47b6f9ca8f900e0415e2e31d75
jocee <jocee@LAPTOP-QT0BSSTR.home>
2021-09-22 18:40:40 +0200

    22-09-2021 APORTE 2.

--- EjercicioBucleFor/src/Tablas.java
+++ EjercicioBucleFor/src/Tablas.java
@@ -5,18 +5,22 @@ public class Tablas {
 		
 	int resultado;
 	String fila;
+	int suma;
 	
 	
 	for(int numero=1;numero<=9;numero++) {
-		
+		System.out.println("\nTabla del "+numero+":");
+		suma=0;
 		
 		for(int j=1;j<=10;j++) {
 			resultado = numero*j;
+			suma = suma+resultado;
 			fila = numero + "  x   "+ j+ "   =   "+ resultado;
 			System.out.println(fila);
 		}
 		
-		System.out.println("******************");
+		System.out.println("********"+" La suma vale: "+suma+"**********");
+
 	}
 	}
 
