1d5c26481e0f1fa144b2a63c2f206c6d61fab510
jocee <jocee@LAPTOP-QT0BSSTR.home>
2021-09-22 11:34:52 +0200

    22-09-2021   APORTE 1.

---
+++ Ejercicio3/src/paquete/Clase.java
@@ -0,0 +1,59 @@
+package paquete;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Clase {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	String nombre;
+	int anoNacimiento;
+	int suma=0; //acumulador
+	float media;
+	int edad;
+	int anoAhora = Calendar.getInstance().get(Calendar.YEAR);
+	int menor=Integer.MAX_VALUE;
+	boolean continuar = true;
+	String nombreMenor = null;
+	int contEdad=0;
+	String mensaje;
+	
+	do {
+		System.out.println("Introduzca nombre:");
+		nombre=sc.nextLine();
+		if(nombre.equals("fin")) {
+			
+			continuar=false;
+			
+		}else {
+			
+			System.out.println("Introduzca un año de nacimiento:");
+			anoNacimiento=Integer.valueOf(sc.nextLine());
+			edad = anoAhora - anoNacimiento;
+			contEdad++;
+			suma+=edad;
+			
+			if(edad<menor) {
+				menor = edad;
+				nombreMenor = nombre;
+			}
+		}
+		
+		
+	}
+	while(continuar==true);
+	sc.close();
+	
+	if(contEdad>0) {
+		media = (float)suma/(float)contEdad;
+		
+		mensaje="El mas joven se llama "+nombreMenor+" y la media de la edad es "+ media;
+		System.out.println(mensaje);
+	}
+	
+
+	}
+}
